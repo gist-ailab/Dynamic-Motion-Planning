942f9575118533a175d96e7969358752c7375af8
raeyo <raeyo.kang@gmail.com>
2021-02-19 16:29:58 +0900

    todo: get_random_config

--- environments/dynamic_2d.py
+++ environments/dynamic_2d.py
@@ -1,15 +1,8 @@
-
-from environments.environment import Environment
-
-class Dynamic2D(Environment):
-    pass
-
-
 import matplotlib.pyplot as plt
 import numpy as np
-from Dynamic_2D_env.entities import *
+from .entities import Zone, Obstacle
 
-class Simple2D():
+class Dynamic2D():
     def __init__(self):
         self.reset()
 
@@ -43,31 +36,10 @@ class Simple2D():
         plt.imshow(env)
         plt.pause(0.01)
         
+    def get_random_config(self):
+        """extract non collision config in workspace
 
-if __name__ == '__main__':
-    simple2d = Simple2D()
+        """
+        config = None
+        return config
 
-    for i in range(10000):
-        simple2d.visualize(i)
-
-
-
-    # env = np.zeros([200, 200, 3], np.uint8)
-    # start = Zone(100, 200, size = [20,20], sim= None)
-    # end = Zone(100, 0, size=[20,20], sim= None)
-
-    # obs1 = Obstacle(25, 15, size=[20,20], transform=[10, 0], sim= None)
-    # obs2 = Obstacle(180, 100, size=[10, 10], transform=[-10,0], sim=None)
-
-    # for i in range(100):
-    #     plt.cla()      
-    #     start.draw(env)
-    #     end.draw(env)
-    #     obs1.draw(env)
-    #     obs2.draw(env)
-
-    #     obs1.cycle()
-    #     obs2.cycle()
-
-    #     plt.imshow(env)
-    #     plt.pause(0.01)
